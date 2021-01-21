978ad8ee39508887b1ba07e5a09fc25b74732cba
Ibnu Adam Syah <ibnuadamsyah19@gmail.com>
2021-01-21 21:44:28 +0700

    Update pengan_m.php
    4 - ganti pengan_m ke Pengan_m

--- application/models/pengan_m.php
+++ application/models/pengan_m.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-    class pengan_m extends CI_Model {
+    class Pengan_m extends CI_Model {
     	
 	function __construct()
 	{
