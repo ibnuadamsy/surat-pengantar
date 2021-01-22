0d297b92b7baba89d2dc79035a1c3fb2fb4ea99d
Ibnu Adam Syah <ibnuadamsyah19@gmail.com>
2021-01-22 15:25:51 +0700

    Update config.php

--- administrator/application/config/config.php
+++ administrator/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Apl_pengantar/administrator';
+$config['base_url'] = 'http://surat-pengantar-pejuang.herokuapp.com/administrator/admin';
 
 /*
 |--------------------------------------------------------------------------
