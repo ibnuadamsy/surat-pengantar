5b2ba1e2dee6ad670c59579506361745b9bac912
Ibnu Adam Syah <ibnuadamsyah19@gmail.com>
2021-01-23 13:33:58 +0700

    Update and rename user_c.php to User_c.php

--- administrator/application/controllers/User_c.php
+++ administrator/application/controllers/User_c.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class user_c extends AUTH_Controller {
+class User_c extends AUTH_Controller {
 	var $template='template/index';
 
 	function __construct(){
